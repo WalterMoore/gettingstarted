78eff60dedf729b486af0638afa4b1bb5e359c5f
bamaswampmonster <bamaswampmonster@bamaswampmonsters-MacBook-Pro.local>
2015-10-07 10:40:11 -0500

    update

--- app/src/app/app.ts
+++ app/src/app/app.ts
@@ -1,12 +1,16 @@
 import {App, events, register, routing} from 'platypus';
 import HomeViewControl from '../viewcontrols/home/home.vc';
+import RegisterViewControl from '../viewcontrols/register/register.vc';
+import LoginViewControl from '../viewcontrols/login/login.vc';
 
 export default class MyApp extends App {
     constructor(router: routing.Router) {
         super();
 
         router.configure([
-            { pattern: '', view: HomeViewControl }
+            { pattern: '', view: HomeViewControl },
+            { pattern: 'login', view: LoginViewControl },
+            { pattern: 'register', view: RegisterViewControl }
         ]);
     }
 
--- app/src/viewcontrols/register/register.vc.ts
+++ app/src/viewcontrols/register/register.vc.ts
@@ -4,12 +4,30 @@ import HomeViewControl from '../home/home.vc';
 import UserRepository from '../../repositories/user/user.repo';
 
 export default class RegisterViewControl extends BaseViewControl {
+        constructor(private userRepository: UserRepository) {
+        super();
+    }
     templateString: string = require('./register.vc.html');
-
-    context: any = {};
+    register(): void {
+        this.context.error = '';
+        this.userRepository.register(this.context.email,
+            this.context.password,
+            this.context.firstname,
+            this.context.lastname)
+        .then((success) => {
+            this.navigator.navigate(HomeViewControl);
+        }).catch((error) => {
+            this.context.error = error;
+        });
+    }
+    context: contexts.IRegister = {
+        firstname: '',
+        lastname: '',
+        email: '',
+        password: '',
+        error: ''
+    };
 }
+
 
 register.viewControl('register-vc', RegisterViewControl, [UserRepository]);
-        constructor(private userRepository: UserRepository) {
-    super();
-}
