7d4eb254a96e1549e65f478c2feda62c3d6cc970
bamaswampmonster <bamaswampmonster@bamaswampmonsters-MacBook-Pro.local>
2015-10-07 11:49:32 -0500

    clean up

--- app/src/viewcontrols/home/home.vc.ts
+++ app/src/viewcontrols/home/home.vc.ts
@@ -3,7 +3,12 @@ import BaseViewControl from '../base/base.vc';
 
 export default class HomeViewControl extends BaseViewControl {
     templateString: string = require('./home.vc.html');
-
+    canNavigateTo(): boolean {
+        if(this.userRepository.userid === 0) {
+            this.navigator.navigate('login-vc');
+            return false;
+        }
+    }
     context: any = {};
 }
 
